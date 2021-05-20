aea5eec31b95a31d4399c305c2e34553ec1fb0cd
nakano.takuho <nakano.takuho@CA-PC-1073.local>
2021-05-20 12:09:48 +0900

    Add pdfbox sample

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,35 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		// SpringApplication.run(DemoApplication.class, args);
+		try (PDDocument doc = new PDDocument()) {
+
+			PDPage page = new PDPage(PDRectangle.A4);
+			doc.addPage(page);
+	
+			setupText(doc, page);
+	
+			doc.save("build/example1.pdf");
+	
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	private static void setupText(PDDocument doc, PDPage page) {
+
+		try (PDPageContentStream content = new PDPageContentStream(doc, page)) {
+	
+			content.beginText();
+	
+			PDFont font = PDType1Font.HELVETICA_BOLD;
+			content.setFont(font, 12);
+			content.newLineAtOffset(100, 700);
+			content.showText("Hello World");
+	
+			content.endText();
+	
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
